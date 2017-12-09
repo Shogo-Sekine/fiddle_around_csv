b1c172a2e2283b52e8d50ee0706f82ea0930c544
Shogo Sekine <gan.gun.kyosho@gmail.com>
2017-12-09 18:02:41 +0900

    qiita用編集

--- data/create_csv.py
+++ data/create_csv.py
@@ -1,10 +1,13 @@
 import csv
 
 def main():
+    # rowで行数、columnで列数を指定
     row = 10
     column = 10
     with open('sample.csv', 'w') as f:
-        f.write('BLANK_,')
+        f.write('BLANK_,') # [0,0]番地は空白
+
+        # ヘッダー名
         for i in range(column):
             f.write('HEADER_' + str(i + 1))
             if (i + 1) != column:
@@ -12,6 +15,7 @@ def main():
         f.write('\n')
 
         for i in range(row):
+            # 各行の先頭にインデックスをふる
             f.write('INDEX_' + str(i + 1))
             f.write(',')
             for j in range(column):
